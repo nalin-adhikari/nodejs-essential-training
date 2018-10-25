adef2e491e739ebb2c1b2db40f11285989a2796f
nalin <nalinadhikariofficial@gmail.com>
2018-10-26 01:41:40 +1100

    file system -> read file

--- filesystem-module/read.js
+++ filesystem-module/read.js
@@ -1,5 +1,14 @@
 var fs = require("fs");
+var path = require("path");
 
-var contents = fs.readFileSync("./lib/newTestFile.txt", "UTF-8");
-
-console.log(contents);
+fs.readdir("./lib", function(err, files){
+    files.forEach(function(fileName){
+        var file = path.join(__dirname, "lib", fileName);
+        var stats = fs.statSync(file);
+        if(stats.isFile() && fileName !== ".DS_Store") {
+            fs.readFile(file, "UTF-8", function(err, contents){
+                console.log(contents);
+            });
+        }
+    });
+});
