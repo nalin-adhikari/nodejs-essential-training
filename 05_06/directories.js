36aa2ac16db47469ea8ff85c12c232e657348948
nalin <nalinadhikariofficial@gmail.com>
2018-10-26 09:51:19 +1100

    removing empty assets directory

--- 05_06/directories.js
+++ 05_06/directories.js
@@ -1,5 +1,21 @@
 var fs = require("fs");
 
-fs.renameSync("./assets/logs", "./logs");
+// fs.renameSync("./assets/logs", "./logs");
 
-console.log("Directory moved.");
+// console.log("Directory moved.");
+
+fs.rmdir("./assets", function(err) {
+    if(err){
+        throw err;
+    }
+
+    console.log("Assets Directory Removed.");
+});
+
+fs.rmdir("./logs", function(err){
+    if(err){
+        throw err;
+    } else {
+        console.log("Logs directory removed.");
+    }
+});
