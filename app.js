553b826786b3a40fbb5826c7586a7e3fd4a41815
nalin <nalinadhikariofficial@gmail.com>
2018-10-25 23:34:10 +1100

    funtion grab the value from process argv array using flag name.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(process.argv)
+function grab(flag){
+    var index = process.argv.indexOf(flag);
+    return (index === -1) ? null : process.argv[index+1];
+}
+
+var greeting = grab('--greeting');
+var user = grab('--user');
+
+if(!user || !greeting) {
+    console.log("You Blew it!");
+} else {
+    console.log(`Welcome ${user}, ${greeting}`);
+}
